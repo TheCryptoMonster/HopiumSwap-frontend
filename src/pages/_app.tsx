7033a29eefdcfc0216e9d00e3a9ec98b8c9fc612
Crypto Monster <66889078+TheCryptoMonster@users.noreply.github.com>
2022-06-09 16:18:26 -0700

    test change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -60,7 +60,7 @@ function MyApp(props: AppProps) {
         <meta name="twitter:image" content="https://twitter.com/HopiumSwap/header_photo" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on Polygon! Earn CAKE through yield farming or win it in the Lottery, then stake it in Hopium Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by HopiumSwap), NFTs, and more, on a platform you can trust."
+          content="The most popular AMM on Polygon! Earn HOPE through yield farming or win it in the Lottery, then stake it in Hopium Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by HopiumSwap), NFTs, and more, on a platform you can trust."
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="" HopiumSwap - A next evolution DeFi exchange on Polygon Smart Chain." />
