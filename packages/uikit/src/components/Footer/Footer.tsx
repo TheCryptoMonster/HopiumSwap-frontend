6e0777d355172b8a63c709cf71a1d8d9c60d7082
Crypto Monster <66889078+TheCryptoMonster@users.noreply.github.com>
2022-06-13 15:43:29 -0700

    cake label

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -96,7 +96,7 @@ const MenuItem: React.FC<FooterProps> = ({
               scale="sm"
               endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
             >
-              {buyHopeLabel}
+              {buyCakeLabel}
             </Button>
           </Flex>
         </StyledToolsContainer>
