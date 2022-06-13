2ac64ae158ad9d1a85ee98944a42a9556d3fd143
Crypto Monster <66889078+TheCryptoMonster@users.noreply.github.com>
2022-06-13 15:40:10 -0700

    fix

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -96,7 +96,7 @@ const MenuItem: React.FC<FooterProps> = ({
               scale="sm"
               endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
             >
-              {buyHOPELabel}
+              {buyHopeLabel}
             </Button>
           </Flex>
         </StyledToolsContainer>
